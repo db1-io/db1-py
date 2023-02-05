612e54fb311384488a1ff267d267ae97a1471078
Anton Eriksson <42644211+antonemanuel@users.noreply.github.com>
2023-02-05 18:21:56 +0100

    Adds exception on general http error (#18)

--- db1/__init__.py
+++ db1/__init__.py
@@ -5,5 +5,12 @@ from db1._item import Item
 from db1.api._item._rest.rest_functions import delete_item, get_item, set_item
 from db1.api._item._websocket.ws_functions import await_next_item, listen
 
-
-__all__ = ["serializer", "Item", "delete_item", "get_item", "set_item", "await_next_item", "listen"]
+__all__ = [
+    "serializer",
+    "Item",
+    "delete_item",
+    "get_item",
+    "set_item",
+    "await_next_item",
+    "listen",
+]
