6fc0e10d7fb27bb2d8fd37647be5386d6b41b2bb
Anton Eriksson <anton.emanuel@icloud.com>
2023-02-27 20:15:46 +0100

    Add JSON encode/decode in CLI

--- db1/_cli/main.py
+++ db1/_cli/main.py
@@ -8,7 +8,14 @@ from db1._cli.item import handle_resource_item
 def main():
     """Main entry point for the DB1 CLI."""
 
-    parser = argparse.ArgumentParser(description="DB1 Command Line Interface (CLI).")
+    DESCRIPTION = (
+        "DB1 Command Line Interface (CLI).\n\n"
+        "On set values are attempted to be decoded as JSON, otherwise as strings.\n"
+        "On get values are attempted to be encoded as JSON, otherwise using the print function.\n"
+    )
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION, formatter_class=argparse.RawTextHelpFormatter
+    )
 
     # Positional arguments
     parser.add_argument(
@@ -36,6 +43,12 @@ def main():
         "--version",
         action="store_true",
         help="Print the version number and exit.",
+    )
+    parser.add_argument(
+        "-pp",
+        "--pretty-print",
+        action="store_true",
+        help="Pretty print the value on get.",
     )
 
     args = vars(parser.parse_args())
