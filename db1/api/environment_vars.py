8b1e240db002b4bbbf6413838390572b3e304c28
Anton Eriksson <42644211+antonemanuel@users.noreply.github.com>
2023-02-27 20:23:08 +0100

    Remove ws url (#24)

--- db1/api/environment_vars.py
+++ db1/api/environment_vars.py
@@ -3,16 +3,11 @@
 import os
 
 DB1_HTTP_URL = "https://item.db1.io"
-DB1_WS_URL = "wss://item-ws.db1.io"
 
 DB1_HTTP_URL_KEY = "DB1_HTTP_URL"
-DB1_WS_URL_KEY = "DB1_WS_URL"
 
 if os.environ.get(DB1_HTTP_URL_KEY) is not None:
     DB1_HTTP_URL = os.environ[DB1_HTTP_URL_KEY]
-
-if os.environ.get(DB1_WS_URL_KEY) is not None:
-    DB1_WS_URL = os.environ[DB1_WS_URL_KEY]
 
 DB1_HTTP_CREATE_ENDPOINT = DB1_HTTP_URL + "/create"
 DB1_HTTP_SET_ENDPOINT = DB1_HTTP_URL + "/set"
