2c639cb22fc56558421c4b89f0f840e860bdf089
Anton Eriksson <anton.emanuel@icloud.com>
2023-03-19 01:32:47 +0100

    Adds dict style interface

--- db1/__init__.py
+++ db1/__init__.py
@@ -1,12 +1,12 @@
 """DB1 python client package."""
 
 from db1 import serializer
-from db1._item import Item
+from db1._client import DB1
 from db1.api._item._rest.rest_functions import delete_item, get_item, set_item
 
 __all__ = [
+    "DB1",
     "serializer",
-    "Item",
     "delete_item",
     "get_item",
     "set_item",
